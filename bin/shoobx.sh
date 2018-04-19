31617afff794a02f95ce7ae1c91c63d97eccd08e
djunh1 <djunh1@gmail.com>
2018-04-19 18:57:46 -0400

    move gem installs to readme

--- bin/shoobx.sh
+++ bin/shoobx.sh
@@ -24,15 +24,3 @@ rvm install 2.3.0
 rvm --default use 2.3.0
 sudo apt install ruby-bundler -y
 sudo apt install ruby-railties -y
-gem install nio4r -v '2.3.0'
-gem install bycrypt -v '3.1.11'
-gem install bindex -v '0.5.0'
-gem install byebug -v '10.0.2'
-gem install ffi -v '1.9.23'
-gem install puma -v '3.11.4'
-gem install sqlite3 -v '1.3.13'
-bundle update sqlite3
-gem install nokogiri -v '1.8.2'
-bundle update nokogiri
-gem install websocket-driver -v '0.6.5'
-gem install rails
--- readme.txt
+++ readme.txt
@@ -26,7 +26,19 @@ Run $ bin/shoobx.sh
 
 Step 4: update the ruby rubygems
 
-$ bundle install
+gem install nio4r -v '2.3.0'
+gem install bycrypt -v '3.1.11'
+gem install bindex -v '0.5.0'
+gem install byebug -v '10.0.2'
+gem install ffi -v '1.9.23'
+gem install puma -v '3.11.4'
+gem install sqlite3 -v '1.3.13'
+bundle update sqlite3
+gem install nokogiri -v '1.8.2'
+bundle update nokogiri
+gem install websocket-driver -v '0.6.5'
+gem install rails
+bundle install
 
 Step 5:
 
